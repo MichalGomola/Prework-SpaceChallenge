1fae0d1b444f62a211c1e3576b6f09fb941cb89c
Michał Gomoła <michal.gomola.89@gmail.com>
2023-01-02 15:33:23 +0100

    Added Simulation class

---
+++ SpaceChallenge/src/Simulation.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+public class Simulation {
+    // Simulation()
+
+    /** method loadItems - loads items from file
+     * @param fileName - String
+     * @param filePath - String
+     *
+     * @return ArrayList<Item>
+     * */
+    public void loadItems(String fileName, String filePath) {
+
+    }
+
+    /** method loadU1 - creating U1 rockets that carry at given load
+     * @param load - ArrayList<Item>
+     *
+     * @return ArrayList<rocket> - ArrayList of U1 rockets with cargo
+     * */
+    public void loadU1(ArrayList load) {
+
+    }
+
+    /** method loadU2 - creating U2 rockets that carry at given load
+     * @param load - ArrayList<Item>
+     *
+     * @return ArrayList<rocket> - ArrayList of U2 rockets with cargo
+     * */
+    public void loadU2(ArrayList load) {
+
+    }
+
+    /** method runSimulation - runs simulation to get cost of mission
+     * @param rockets - ArrayList<Rocket>
+     *
+     * @return int - budget - total cost of sent rockets (including crashed ones)
+     * */
+    public void runSimulation(ArrayList rockets) {
+
+    }
+}
