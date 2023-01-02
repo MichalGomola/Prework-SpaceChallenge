829a9ca12743c8e7dd2ca71d7c7fce79f8ef771c
Michał Gomoła <michal.gomola.89@gmail.com>
2023-01-02 21:01:56 +0100

    Added loadItems method to Simulation class

--- SpaceChallenge/src/Main.java
+++ SpaceChallenge/src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String fileNameOne = "/home/michal/Desktop/Prework-SpaceChallenge/SpaceChallenge/src/phase-1.txt";
+        String fileNameTwo = "/home/michal/Desktop/Prework-SpaceChallenge/SpaceChallenge/src/phase-2.txt";
+
+        Simulation simulation = new Simulation();
+
+        ArrayList<Item> itemsOne = simulation.loadItems(fileNameOne);
+        ArrayList<Item> itemsTwo = simulation.loadItems(fileNameTwo);
+
+        for (Item i : itemsOne) {
+            System.out.println(i.name + " weights: " + i.weight + "kg");
+        }
+
+        for (Item i : itemsTwo) {
+            System.out.println(i.name + " weights: " + i.weight + "kg");
+        }
     }
 }
--- SpaceChallenge/src/Simulation.java
+++ SpaceChallenge/src/Simulation.java
@@ -1,21 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+
+
 public class Simulation {
-    // Simulation()
 
     /** method loadItems - loads items from file
      * @param fileName - String
-     * @param filePath - String
      *
      * @return ArrayList<Item>
      * */
-    public void loadItems(String fileName, String filePath) {
+    public ArrayList<Item> loadItems(String fileName) throws Exception{
 
+        Path path = Paths.get(fileName);
+        ArrayList<Item> items = new ArrayList<Item>();
+
+        try (FileReader fr = new FileReader(fileName, StandardCharsets.UTF_8);
+             BufferedReader reader = new BufferedReader(fr)) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] item = line.split("=");
+                Item newItem = new Item(item[0], Integer.parseInt(item[1]));
+                items.add(newItem);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return items;
     }
 
     /** method loadU1 - creating U1 rockets that carry at given load
      * @param load - ArrayList<Item>
      *
-     * @return ArrayList<rocket> - ArrayList of U1 rockets with cargo
+     * @return ArrayList<Rocket> - ArrayList of U1 rockets with cargo
      * */
     public void loadU1(ArrayList load) {
 
