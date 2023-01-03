93c58a041c0490de2d64956dece13dc5d300f13d
Michał Gomoła <michal.gomola.89@gmail.com>
2023-01-03 13:19:54 +0100

    updated Simulation methods

--- SpaceChallenge/src/Simulation.java
+++ SpaceChallenge/src/Simulation.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 
 
@@ -16,8 +14,7 @@ public class Simulation {
      * */
     public ArrayList<Item> loadItems(String fileName) throws Exception{
 
-        Path path = Paths.get(fileName);
-        ArrayList<Item> items = new ArrayList<Item>();
+        ArrayList<Item> items = new ArrayList<>();
 
         try (FileReader fr = new FileReader(fileName, StandardCharsets.UTF_8);
              BufferedReader reader = new BufferedReader(fr)) {
@@ -39,25 +36,68 @@ public class Simulation {
      *
      * @return ArrayList<Rocket> - ArrayList of U1 rockets with cargo
      * */
-    public void loadU1(ArrayList load) {
+    public ArrayList<Rocket> loadU1(ArrayList<Item> load) {
 
+        ArrayList<Rocket> u1Rockets = new ArrayList<>();
+        Rocket u1 = new U1();
+        u1Rockets.add(u1);
+
+        while (load.size() != 0) {
+            if (u1Rockets.get(u1Rockets.size()-1).canCarry(load.get(0))) {
+                u1Rockets.get(u1Rockets.size()-1).carry(load.get(0));
+                load.remove(0);
+            } else {
+                u1 = new U1();
+                u1Rockets.add(u1);
+            }
+        }
+        return u1Rockets;
     }
 
     /** method loadU2 - creating U2 rockets that carry at given load
      * @param load - ArrayList<Item>
      *
-     * @return ArrayList<rocket> - ArrayList of U2 rockets with cargo
+     * @return ArrayList<Rocket> - ArrayList of U2 rockets with cargo
      * */
-    public void loadU2(ArrayList load) {
+    public ArrayList<Rocket> loadU2(ArrayList<Item> load) {
 
+        ArrayList<Rocket> u2Rockets = new ArrayList<>();
+        Rocket u2 = new U2();
+        u2Rockets.add(u2);
+
+        while (load.size() != 0) {
+            if (u2Rockets.get(u2Rockets.size()-1).canCarry(load.get(0))) {
+                u2Rockets.get(u2Rockets.size()-1).carry(load.get(0));
+                load.remove(0);
+            } else {
+                u2 = new U2();
+                u2Rockets.add(u2);
+            }
+        }
+        return u2Rockets;
     }
+
 
     /** method runSimulation - runs simulation to get cost of mission
      * @param rockets - ArrayList<Rocket>
      *
      * @return int - budget - total cost of sent rockets (including crashed ones)
      * */
-    public void runSimulation(ArrayList rockets) {
+    public int  runSimulation(ArrayList<Rocket> rockets) {
+        int budget = 0;
 
+        while (rockets.size() != 0) {
+            if (rockets.get(0).launch()) {
+                if (rockets.get(0).land()) {
+                    budget += rockets.get(0).cost;
+                    rockets.remove(0);
+                } else {
+                    budget += rockets.get(0).cost;
+                }
+            } else {
+                budget += rockets.get(0).cost;
+            }
+        }
+        return budget;
     }
 }
