80685cde8caaf241c2b4ceae74f75571b8643bb5
Michał Gomoła <michal.gomola.89@gmail.com>
2023-01-03 13:21:16 +0100

    Added program logic at Main.java

--- SpaceChallenge/src/Main.java
+++ SpaceChallenge/src/Main.java
@@ -7,15 +7,28 @@ public class Main {
 
         Simulation simulation = new Simulation();
 
-        ArrayList<Item> itemsOne = simulation.loadItems(fileNameOne);
-        ArrayList<Item> itemsTwo = simulation.loadItems(fileNameTwo);
+        ArrayList<Rocket> u1RocketsOne = simulation.loadU1(simulation.loadItems(fileNameOne));
+        ArrayList<Rocket> u1RocketsTwo = simulation.loadU1(simulation.loadItems(fileNameTwo));
+        
+        ArrayList<Rocket> u2RocketsOne = simulation.loadU2(simulation.loadItems(fileNameOne));
+        ArrayList<Rocket> u2RocketsTwo = simulation.loadU2(simulation.loadItems(fileNameTwo));
 
-        for (Item i : itemsOne) {
-            System.out.println(i.name + " weights: " + i.weight + "kg");
-        }
+        int u1BudgetOne = simulation.runSimulation(u1RocketsOne);
+        int u1BudgetTwo = simulation.runSimulation(u1RocketsTwo);
 
-        for (Item i : itemsTwo) {
-            System.out.println(i.name + " weights: " + i.weight + "kg");
-        }
+
+        int u2BudgetOne = simulation.runSimulation(u2RocketsOne);
+        int u2BudgetTwo = simulation.runSimulation(u2RocketsTwo);
+
+
+        System.out.println("Budget for Phase-1 with U1 rockets: $" + u1BudgetOne);
+        System.out.println("Budget for Phase-2 with U1 rockets: $" + u1BudgetTwo);
+        System.out.println("Total budget with U1 rockets: $" + (u1BudgetOne + u1BudgetTwo));
+        System.out.println();
+        System.out.println("Budget for Phase-1 with U2 rockets: $" + u2BudgetOne);
+        System.out.println("Budget for Phase-2 with U2 rockets: $" + u2BudgetTwo);
+        System.out.println("Total budget with U2 rockets: $" + (u2BudgetOne + u2BudgetTwo));
+
+
     }
 }
